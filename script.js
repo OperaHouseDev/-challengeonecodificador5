f680b865fddf5e7d60e29d977d3034669dcba934
Jose Correa <correa.jose@correounivalle.edu.co>
2023-09-22 00:34:58 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+const txtinput = document.querySelector(".input");
+const txtoutput = document.querySelector(".output");
+const container = document.querySelector(".container");
+const btncopy = document.querySelector(".btn-copy");
+const regex = /^[a-z]/; // solo minusculas, sin acentos y sin caracteres especiales
+const keys = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]]; // codigos para encriptar vocales
+
+btncopy.style.visibility= "hidden";
+
+function validateText(){
+    let txtValue = document.querySelector(".input").value;
+    if (!regex.test(txtValue)){ 
+        alert("ERROR! No se permite el uso de letras mayusculas, con acento o caracteres especiales")
+        location.reload();
+        return true
+    }
+}
+
+function btnEncrypt(){
+    if(!validateText()){
+        const encryptedtxt = Encrypt(txtinput.value);
+        txtinput.value = "";
+        txtoutput.value = encryptedtxt;
+        container.style.backgroundImage= "none";
+        btncopy.style.visibility= "visible";
+    }
+}
+
+function btnDecrypt(){
+    if(!validateText()){
+        const decryptedtxt = Decrypt(txtinput.value);
+        txtinput.value = "";
+        txtoutput.value = decryptedtxt;
+        container.style.backgroundImage= "none";
+        btncopy.style.visibility= "visible";
+    }
+}
+
+function copyText(){
+    txtoutput.select();
+    navigator.clipboard.writeText(txtoutput.value)
+    txtinput.value = "";
+    txtoutput.value = "";
+    container.style.backgroundImage= "url('Muñeco.png')";
+}
+
+function Encrypt(txt){
+    for (let i = 0; i< keys.length; i++){
+        if ( txt.includes(keys[i][0])){
+            txt = txt.replaceAll(keys[i][0],keys[i][1])
+        }
+    }
+    return txt
+}
+
+function Decrypt(txt){
+    for (let i = 0; i< keys.length; i++){
+        if ( txt.includes(keys[i][1])){
+            txt = txt.replaceAll(keys[i][1],keys[i][0])
+        }
+    }
+    return txt
+}
+
+document.addEventListener('keyup', function(event) {
+    const key = event.key; 
+    if (key === "Delete" || key === "Backspace") {
+        if (txtoutput.value == ""){
+            container.style.backgroundImage= "url('Muñeco.png')";
+        }
+    }
+});
